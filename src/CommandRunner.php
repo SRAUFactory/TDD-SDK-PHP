64c843e44ee6d9e8d28d0cb40ec0bd956c4b9d7b
SRAUFactory <srau.factory@gmail.com>
2016-10-31 21:47:51 +0900

    Add CommandRunner

---
+++ src/CommandRunner.php
@@ -0,0 +1,23 @@
+<?php
+namespace Tdd;
+class CommandRunner {
+    /**
+     * main function
+     */ 
+    public static function main() {
+         $runner = new static;
+         echo get_class($runner). "\n";
+         return $runner->run($_SERVER['argv']);
+    }
+
+    /**
+     * run command
+     * @param array $argv
+     * @return 
+     */ 
+    public function run(array $argv) {
+        // @ToDo Run each command
+        return true;
+    }
+}
+

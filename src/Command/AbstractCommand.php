1fc5a8a37346257071f10b87a8890617c841b68e
SRAUFactory <srau.factory@gmail.com>
2016-11-02 20:23:46 +0900

    Design fixed

--- src/Command/AbstractCommand.php
+++ src/Command/AbstractCommand.php
@@ -1,13 +1,81 @@
 <?php
 namespace Tdd\Command;
 /**
- * TDD Command Base Class
+ * The base class of TDD command.
  * To inherit this class when TDD command implement.
  */
 abstract class AbstractCommand {
     /**
-     * Create Command
+     * Target class
+     * @var ReflectionClass
+     */
+    protected $target;
+    /**
+     * Command output file name
+     * @var string
+     */ 
+    protected $outputFileName;
+
+    /**
+     * Constructor
      * @param array $params
      */ 
-    abstract public function create(array $params);
+    public function __construct(array $params) {
+        $this->parseParameters($params);
+    }
+
+    /**
+     * Create command
+     * @return boolean true is success to create.
+     */ 
+    abstract public function create();
+
+    /**
+     * The values bind on template.
+     * @param string $templateName Template file name
+     * @param array $values The values to bind
+     * @return string After bind on template value.
+     */ 
+    protected function bindTemplate($templateName, array $values) {
+        $bindValues = file_get_contents("template/{$templateName}.txt");
+        foreach ($values as $key => $value) {
+            $bindValues = str_replace("###{$key}###", $value, $bindValues);
+        }
+        return $bindValues;
+    }
+
+    /**
+     * Output file
+     * @param string $value Output value
+     */ 
+    protected function outputFile($value) {
+         file_put_contents($this->outputFileName, $value);
+    }
+
+    /**
+     * Is output method
+     * @param ReflectionMethod $method Target method
+     * @return boolean True is output method 
+     */ 
+    protected function isOutputMethod(ReflectionMethod $method) {
+        return $this->target->getName() === $method->class
+            && $method->isPublic();
+    }
+
+    /**
+     * Parse parameter values
+     * @param array $params
+     */
+    private function parseParameters(array $params) {
+        if (!empty($params["bootstrap"])) {
+            require($params["bootstrap"]);
+        }
+
+        $this->target = new ReflectionClass($params["classname"]);
+
+        $this->outputFileName = $params["output"]. "/". $this->target->getShortName() . "Test.php";
+        if (empty($params["output"])) {
+           $this->outputFileName = str_replace(".php", "Test.php", $this->target->getFileName());
+        }
+    }
 }
--- src/Runner/CommandRunner.php
+++ src/Runner/CommandRunner.php
@@ -1,23 +1,81 @@
 <?php
 namespace Tdd\Runner;
+use \Exception;
+/**
+ * Run comannd batch
+ * When you execute for CLI, this class use.
+ */
 class CommandRunner {
+    /**
+     * Command Name
+     * @value [create]
+     * @var string
+     */ 
+    private $command;
+
+    /**
+     * Target Name
+     * @value [test/source/doc]
+     */ 
+    private $target;
+    private $options = [];
+
     /**
      * main function
      */ 
     public static function main() {
          $runner = new static;
-         echo get_class($runner). "\n";
          return $runner->run($_SERVER['argv']);
     }
 
     /**
      * run command
      * @param array $argv
-     * @return 
+     * @return boolean The result to run command
      */ 
     public function run(array $argv) {
-        // @ToDo Run each command
-        return true;
+        // Analyze $argv to Command options
+        $this->parseArguments($argv);
+
+        // Run each command
+        $commandClass = $this->getCommandClass();
+        return $commandClass->{$this->target}();
+    }
+    
+    /**
+     * Parse arguments
+     * @param array $argv the arguments to run command.
+     */ 
+    private function parseArguments(array $argv) {
+         if (count($argv) <= 3) {
+             throw new Exception("Argument is missing.");
+         }
+
+         $this->command = $argv[1];
+         $this->target  = $argv[2];
+
+         unset($argv[0]);
+         unset($argv[1]);
+         unset($argv[2]);
+
+         $this->options = $argv; 
+    }
+
+    /**
+     * Get command class.
+     * @return string;
+     */ 
+    private function getCommandClass() {
+        switch ($this->command) {
+            case "source" :
+            //    retrun new SourceCode();
+            case "test" :
+            //    return new TestCase();
+            case "doc" :
+            //    return new Document();
+            default :
+                throw new Exception("No such command!!");
+        }
     }
 }
 

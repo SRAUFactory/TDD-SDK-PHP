f72b444777a95d70bfa00fea65027fa505a06f98
SRAUFactory <srau.factory@gmail.com>
2016-11-20 11:58:40 +0900

    Function divided
    Function divided to use by ‘array_walk’.

--- src/Command/TestCode.php
+++ src/Command/TestCode.php
@@ -5,6 +5,8 @@ namespace Tdd\Command;
  */
 class TestCode extends AbstractCommand {
     private $functions = ""; 
+    private $params = [];
+    private $docs = "";
 
     /**
      * @override
@@ -13,14 +15,12 @@ class TestCode extends AbstractCommand {
     public function create() {
          $methods = $this->target->getMethods();
          array_walk($methods, [$this, 'setFunctions']);
-
          $values = [
              "className" => $this->target->getName(),
              "shortName" => $this->target->getShortName(),
              "testFunctions" => $this->functions,
          ];
-         $outputTestCase = $this->bindTemplate("TestCase", $values);
-         $this->outputFile($outputTestCase);
+         $this->outputFile($this->bindTemplate("TestCase", $values));
          return true;
     }
 
@@ -34,33 +34,35 @@ class TestCode extends AbstractCommand {
              return;
          }   
          
-         // @ToDo Replace to Document.php
-         $outputParams = []; 
-         $outputDocs = "";
-         foreach ($method->getParameters() as $parameter) {
-             $name = "$". $parameter->name;
-             $outputDocs .= "\n     * @param string {$name} any param";
-             $outputParams[] = $name; 
-         }   
-         
+         $parameters = $method->getParameters();
+         array_walk($parameters, [$this, 'setParameters']); 
          $paramValues = [
              "name" => $method->name,
              "largeName" => ucfirst($method->name),
-             "docs" => $outputDocs,
-             "params" => implode(", ", $outputParams),
+             "docs" => $this->docs,
+             "params" => implode(", ", $this->params),
              "className" => $this->target->getShortName(),
          ];  
          $templateName = ($method->isStatic())? "TestStaticFunction" : "TestFunction";
          
          $outputProvider = "";
-         if (count($outputParams) > 0) {
+         if (count($this->params) > 0) {
              $outputProvider = $this->bindTemplate("TestProvider", $paramValues);
              preg_match('/(function )[a-zA-z0-9:punct:]*/', $outputProvider, $matches);
              $providerName = str_replace($matches[1], "", $matches[0]);
              $paramValues["docs"] = "@dataProvider {$providerName}" . $paramValues["docs"];
-         }   
-         
-         $this->functions .= $this->bindTemplate($templateName, $paramValues);
-         $this->functions .= $outputProvider;
+         }    
+         $this->functions .= $this->bindTemplate($templateName, $paramValues). $outputProvider;
+    }
+
+    /**
+     * Set test parameters
+     * @param ReflectionParameter $parameter
+     * @param int $index The index of Parameter List
+     */
+    private function setParameters($parameter, $index) {
+        $name = "$". $parameter->name;
+        $this->docs .= "\n     * @param string {$name} any param";
+        $this->params[] = $name;
     }
 }

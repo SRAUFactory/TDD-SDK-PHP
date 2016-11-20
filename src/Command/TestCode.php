33e46a1ddad517268acc140e833c1bebe06470fe
SRAUFactory <srau.factory@gmail.com>
2016-11-20 11:44:48 +0900

    Function divided
    Function divided to use by ‘array_walk’.

--- src/Command/TestCode.php
+++ src/Command/TestCode.php
@@ -4,55 +4,63 @@ namespace Tdd\Command;
  * The class to generate Test Code
  */
 class TestCode extends AbstractCommand {
+    private $functions = ""; 
+
     /**
      * @override
      * @return boolean true is success to create.
      */ 
     public function create() {
-         $outputFunctions = "";
          $methods = $this->target->getMethods();
-         foreach ($methods as $method) {
-             if (!$this->isOutputMethod($method)) {
-                 continue;
-             }
- 
-             // @ToDo Replace to Document.php
-             $outputParams = [];
-             $outputDocs = "";
-             foreach ($method->getParameters() as $parameter) {
-                 $name = "$". $parameter->name;
-                 $outputDocs .= "\n     * @param string {$name} any param";
-                 $outputParams[] = $name;
-             }
-
-             $paramValues = [
-                 "name" => $method->name,
-                 "largeName" => ucfirst($method->name),
-                 "docs" => $outputDocs,
-                 "params" => implode(", ", $outputParams),
-                 "className" => $this->target->getShortName(),
-             ];
-             $templateName = ($method->isStatic())? "TestStaticFunction" : "TestFunction";
-
-             $outputProvider = "";
-             if (count($outputParams) > 0) {
-                 $outputProvider = $this->bindTemplate("TestProvider", $paramValues);
-                 preg_match('/(function )[a-zA-z0-9:punct:]*/', $outputProvider, $matches);
-                 $providerName = str_replace($matches[1], "", $matches[0]);
-                 $paramValues["docs"] = "@dataProvider {$providerName}" . $paramValues["docs"];
-             }
-
-             $outputFunctions .= $this->bindTemplate($templateName, $paramValues);
-             $outputFunctions .= $outputProvider;
-         }
+         array_walk($methods, [$this, 'setFunctions']);
 
          $values = [
              "className" => $this->target->getName(),
              "shortName" => $this->target->getShortName(),
-             "testFunctions" => $outputFunctions,
+             "testFunctions" => $this->functions,
          ];
          $outputTestCase = $this->bindTemplate("TestCase", $values);
          $this->outputFile($outputTestCase);
          return true;
+    }
+
+    /**
+     * Set test functions
+     * @param ReflectionMethod $method Target Method
+     * @param int $index The index of Method List
+     */ 
+    private function setFunctions($method, $index) {
+         if (!$this->isOutputMethod($method)) {
+             return;
+         }   
+         
+         // @ToDo Replace to Document.php
+         $outputParams = []; 
+         $outputDocs = "";
+         foreach ($method->getParameters() as $parameter) {
+             $name = "$". $parameter->name;
+             $outputDocs .= "\n     * @param string {$name} any param";
+             $outputParams[] = $name; 
+         }   
+         
+         $paramValues = [
+             "name" => $method->name,
+             "largeName" => ucfirst($method->name),
+             "docs" => $outputDocs,
+             "params" => implode(", ", $outputParams),
+             "className" => $this->target->getShortName(),
+         ];  
+         $templateName = ($method->isStatic())? "TestStaticFunction" : "TestFunction";
+         
+         $outputProvider = "";
+         if (count($outputParams) > 0) {
+             $outputProvider = $this->bindTemplate("TestProvider", $paramValues);
+             preg_match('/(function )[a-zA-z0-9:punct:]*/', $outputProvider, $matches);
+             $providerName = str_replace($matches[1], "", $matches[0]);
+             $paramValues["docs"] = "@dataProvider {$providerName}" . $paramValues["docs"];
+         }   
+         
+         $this->functions .= $this->bindTemplate($templateName, $paramValues);
+         $this->functions .= $outputProvider;
     }
 }

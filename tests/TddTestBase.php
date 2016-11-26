45ed533d32f3750bfa108ef81b7beb3875a3ec6e
SRAUFactory <srau.factory@gmail.com>
2016-11-26 19:53:57 +0900

    Change to output destination
    Changing the output destination of a file.
    #2

--- tests/TddTestBase.php
+++ tests/TddTestBase.php
@@ -12,11 +12,21 @@ class TddTestBase extends PHPUnit_Framework_TestCase {
     protected $target;
 
     /**
+     * Directories for system
+     */ 
+    private static $SYSTEM_DIR = ["src", "tests", "templates"];
+
+    /**
      * @override
      * @see
      */
     public function setUp() {
         parent::setUp();
+
+        $dir = getenv(TEST_OUTPUT_DIR);
+        if (!file_exists($dir)) {
+            mkdir($dir);
+        }
     }
  
     /**
@@ -26,5 +36,22 @@ class TddTestBase extends PHPUnit_Framework_TestCase {
     public function tearDown() {
         unset($this->target);
         parent::tearDown();
+
+        $dir = getenv(TEST_OUTPUT_DIR);
+        if (file_exists($dir) && !in_array($dir, self::$SYSTEM_DIR)) {
+            $files = scandir($dir);
+            array_walk($files, [$this, "removeFile"], $dir);
+            rmdir($dir);
+        }
     }
+
+    /**
+     * Remove File
+     * @param string $file File Name
+     * @param int $key The index of File List
+     * @param string $dir Target Directory
+     */ 
+    private function removeFile($file, $key, $dir) {
+        unlink("{$dir}/{$file}");
+    } 
 }

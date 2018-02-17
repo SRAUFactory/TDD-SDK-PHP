cf310e99fd7692b313f9676f7df13873716b052e
SRAUFactory <srau.factory@gmail.com>
2018-02-17 23:27:53 +0900

    add: `Tdd\Command\SourceCode`

---
+++ tests/Command/SourceCodeTest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace TddTest\Command;
+
+use Tdd\Command\SourceCode;
+use TddTest\TddTestBase;
+
+/**
+ * Test Case for Tdd\Command\SourceCode.
+ */
+class SourceCodeTest extends TddTestBase
+{
+    /**
+     * Test for create.
+     *
+     * @dataProvider getProvidorCreate
+     *
+     * @param string $className Target Class Name
+     */
+    public function testCreate(string $className)
+    {
+        $dir = getenv(TEST_OUTPUT_DIR);
+        $this->target = new SourceCode(['classname' => $className, 'output' => $dir]);
+        $actual = $this->target->create();
+        $this->assertTrue($actual);
+        // @ToDo Add asserts for output file exists
+    }
+
+    /**
+     * Test Providor for create.
+     *
+     * @return array The list of Test Parameters
+     */
+    public function getProvidorCreate()
+    {
+        return [
+            ['TddTest/Command/TestCodeTest'],
+            ['TddTest/Runner/CommandRunnerTest'],
+        ];
+    }
+}

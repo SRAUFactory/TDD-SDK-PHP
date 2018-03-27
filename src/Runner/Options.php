a6402a7307db5b675eb3a99ed65601626e895262
SRAUFactory <srau.factory@gmail.com>
2018-03-27 19:58:49 +0900

    refactor: added PHPDoc

--- src/Runner/Options.php
+++ src/Runner/Options.php
@@ -21,6 +21,11 @@ class Options
      */
     private $options;
 
+    /**
+     * Set arguments from getopt 
+     *
+     * @return $this
+     */
     public function set() : self
     {
         $this->options = getopt($this->getShortOptions(), $this->getLongOptions());
@@ -28,6 +33,11 @@ class Options
         return $this;
     }
 
+    /**
+     * Get short options.
+     *
+     * @return string Short option value
+     */
     private function getShortOptions() : string
     {
         $result = '';
@@ -38,6 +48,11 @@ class Options
         return $result;
     }
 
+    /**
+     * Get long options.
+     *
+     * @return array Long option values
+     */
     private function getLongOptions() : array
     {
         $result = [];
@@ -48,38 +63,83 @@ class Options
         return $result;
     }
 
+    /**
+     * Check set value.
+     *
+     * @param string $key Target
+     *
+     * @return bool True is to set to options
+     */
     public function isset(string $key) : bool
     {
         return array_key_exists($key, $this->options)
            || array_key_exists($this->getShortOptionKey($key), $this->options);
     }
 
+    /**
+     * Get value.
+     *
+     * @param string $key Target
+     *
+     * @return string value from options
+     */
     public function get(string $key) : string
     {
         return $this->options[$key] ?? $this->options[$this->getShortOptionKey($key)] ?? '';
     }
 
+    /**
+     * Get all values.
+     *
+     * @return array all values
+     */
     public function getValues() : array
     {
         return $this->options;
     }
 
+    /**
+     * Get option key shufix
+     *
+     * @param string $key Target
+     *
+     * @return string option key shufix(`:`, '')
+     */
     private function getOptionKeyShufix(string $key) : string
     {
         return $this->isArgValueRequired($key) ? ':' : '';
     }
 
+    /**
+     * Get short option key
+     *
+     * @param string $key Target
+     *
+     * @return string short option key(ex: self::KEY_GENERATE => 'g')
+     */
     private function getShortOptionKey(string $key) : string
     {
         return substr($key, 0, 1);
     }
 
+    /**
+     * Get all supported option keys.
+     *
+     * @return array all supported option keys
+     */
     private function getOptionKeys() : array
     {
         $reflect = new ReflectionClass($this);
         return $reflect->getConstants();
     }
 
+    /**
+     * Check argument value required.
+     *
+     * @param string $key Target
+     *
+     * @return bool True is required
+     */
     private function isArgValueRequired(string $key) : bool
     {
         return $key !== self::KEY_HELP;

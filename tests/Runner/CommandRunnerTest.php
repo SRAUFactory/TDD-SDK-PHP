12abf290dbaa490550f382ecbd836fa5e7de2630
SRAUFactory <srau.factory@gmail.com>
2018-05-02 17:58:46 +0900

    feat: fixed `TddTest\Runner\CommandRunnerTest`

--- tests/Runner/CommandRunnerTest.php
+++ tests/Runner/CommandRunnerTest.php
@@ -2,8 +2,11 @@
 
 namespace TddTest\Runner;
 
+use Tdd\Command\Options;
 use Tdd\Runner\CommandRunner;
 use TddTest\TddTestBase;
+use \Exception;
+use \InvalidArgumentException;
 
 /**
  * Test Case for Tdd\Runner\CommandRunner.
@@ -15,16 +18,76 @@ class CommandRunnerTest extends TddTestBase
      */
     public function testMain()
     {
-        $this->expectException('InvalidArgumentException');
-        $this->expectExceptionMessage('Argument is missing.');
+        $this->setExpectException(new InvalidArgumentException('Argument is missing.'));
         CommandRunner::main();
     }
 
-    public function testRun()
+    /**
+     * Test for run.
+     *
+     * @dataProvider getProvidorRun
+     *
+     * @param Options $options Arguments for execute
+     */
+    public function testRun(Options $options)
     {
-        $options = new OptionsMock([OptionsMock::KEY_GENERATE => 'mock']);
-        $this->expectException('InvalidArgumentException');
-        $this->expectExceptionMessage('No such command!!');
+        $this->assertTrue(CommandRunner::run($options));
+    }
+
+    /**
+     * Test Providor for run.
+     *
+     * @return array The list of Test Parameters
+     */
+    public function getProvidorRun() : array
+    {
+        $testPattern = [];
+        foreach (CommandRunner::SUPPORTED_CLASSES as $generateKey => $class) {
+            $testPattern[] = [new OptionsMock([
+                OptionsMock::KEY_GENERATE => $generateKey,
+                OptionsMock::KEY_INPUT    => $class,
+                OptionsMock::KEY_OUTPUT   => getenv(TEST_OUTPUT_DIR), 
+            ])];
+        }
+
+        return $testPattern;
+    }
+
+    /**
+     * Test case of throw exception for run.
+     *
+     * @dataProvider getProvidorRunForThrowException
+     *
+     * @param Options   $options  Arguments for execute
+     * @param Exception $expected Expected exception
+     */
+    public function testRunForThrowException(Options $options, Exception $expected)
+    {
+        $this->setExpectException($expected);
         CommandRunner::run($options);
+    }
+
+    /** 
+     * Test Providor for run.
+     *  
+     * @return array The list of Test Parameters
+     */
+    public function getProvidorRunForThrowException() : array
+    {
+        return [
+            [new Options(), new InvalidArgumentException('Argument is missing.')],
+            [new OptionsMock([OptionsMock::KEY_HELP => false]), new InvalidArgumentException('No such command!!')]
+        ];
+    }
+
+    /**
+     * Set expect exception.
+     *
+     * @param Exception $expected Expected exception
+     */ 
+    private function setExpectException(Exception $expected)
+    {
+        $this->expectException(get_class($expected));
+        $this->expectExceptionMessage($expected->getMessage());
     }
 }

76924e3837779769ec35115cd490ee641a220c56
SRAUFactory <srau.factory@gmail.com>
2018-05-02 14:01:07 +0900

    feat: [WIP]re-add `testRun`

--- tests/Runner/CommandRunnerTest.php
+++ tests/Runner/CommandRunnerTest.php
@@ -3,6 +3,7 @@
 namespace TddTest\Runner;
 
 use Tdd\Runner\CommandRunner;
+use TddTest\Runner\OptionsMock;
 use TddTest\TddTestBase;
 
 /**
@@ -18,5 +19,13 @@ class CommandRunnerTest extends TddTestBase
         $this->expectException('InvalidArgumentException');
         $this->expectExceptionMessage('Argument is missing.');
         CommandRunner::main();
+    }
+
+    public function testRun()
+    {
+        $options = new OptionsMock([OptionsMock::KEY_GENERATE => 'mock']);
+        $this->expectException('InvalidArgumentException');
+        $this->expectExceptionMessage('No such command!!');
+        CommandRunner::run($options);
     }
 }
